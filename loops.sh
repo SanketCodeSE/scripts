5f623bb5cd7d684bae2b730b4a4b32d0ee712161
SanketCodeSE <sanketpatel9155@gmail.com>
2025-05-30 11:04:24 +0000

    local Repo

---
+++ loops.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+<<info
+
+Loops: loops is a anything that you want to repeat again and again and again and again based on
+certain conditon 
+for loops considitions
+
+info
+
+
+for (( num=1 ; num<=10 ;  num++ ))
+do 
+	echo "$num"
+	echo "hello"
+
+done
+
+
