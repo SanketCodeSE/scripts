72e67c4a154efc42e3f1a9a7cfddc42999ab9589
SanketCodeSE <sanketpatel9155@gmail.com>
2025-05-31 09:47:46 +0000

    script file

---
+++ hello_chatur.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "chatur: Adarniya sabhapati mahodaya"
+
+echo "chatur: aaj hum sab milke chamatkaar karenge"
+
+echo "rancho: ratta marega to aisa hi hoga"
+
+echo "chatur: thuchuk thuchuk"
+
+
+echo "raju: rancho itna hi hai tujhetoh ek folder banake dikhana"
+
+mkdir -p  rancho
+
+echo "rancho: ye leh, bana diya"
+
+echo "farahan : virus ne bezzati kardi, bola farhanitrate"
+
+echo "faran: abba nahi manenge" >  farahanitrate.txt
+
+
